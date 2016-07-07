e7b2f567850c415ed165faa5253637052cdadeb1
Laurence kite <laurencekite@Laurences-MacBook-Pro-2.local>
2016-07-07 13:23:22 +0100

    Raised and error for full station

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -9,6 +9,7 @@ class DockingStation
   end
 
   def dock(bike)
+    fail 'Docking station full' if @bike
     @bike = bike
   end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -36,7 +36,11 @@ describe DockingStation do
       end
   end
 
-
-  #end
+  describe "#dock" do
+    it 'raises an error when full' do
+       subject.dock(Bike.new)
+       expect {subject.dock Bike.new}.to raise_error 'Docking station full'
+    end
+  end
 
 end

f462bf860c714079c04e5ef96bed1c6455db2327
yurizhuravel <zhuravel@gmail.com>
2016-07-07 16:54:48 +0100

    Adds a test for 20 bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,14 +3,18 @@ require_relative 'bike'
 class DockingStation
   attr_reader :bike
 
+  def initialize
+    @bikes = []
+  end
+
   def release_bike
-    fail 'No bikes available' unless @bike
-    @bike
+    fail 'No bikes available' if @bikes.empty?
+    @bikes.pop
   end
 
   def dock(bike)
-    fail 'Docking station full' if @bike
-    @bike = bike
+    fail 'Docking station full' if @bikes.length > 19
+    @bikes << bike
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -13,7 +13,7 @@ describe DockingStation do
 
   it 'docks bikes' do
     bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
+    expect(subject.dock(bike)).to eq bikes
   end
 
   it 'returnes docked bikes' do
@@ -38,7 +38,7 @@ describe DockingStation do
 
   describe "#dock" do
     it 'raises an error when full' do
-       subject.dock(Bike.new)
+       20.times subject.dock(Bike.new)
        expect {subject.dock Bike.new}.to raise_error 'Docking station full'
     end
   end
